4df6e2cd2cfcb707864100389b255e8219a2b768
HadiAbu <hadi.abuhamed@gmail.com>
2024-01-07 17:29:31 +0100

    started sidebar and added logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,26 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+  HiOutlineHashtag,
+  HiOutlineHome,
+  HiOutlineMenu,
+  HiOutlinePhotograph,
+  HiOutlineUserGroup,
+} from 'react-icons/hi';
+import { RiCloseLine } from 'react-icons/ri';
+import { logo } from '../assets';
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#112227]">
+        <img src={logo} alt="logo" className="w-full h-54 object-contain" />
+        {/* <NavLinks /> */}
+      </div>
+    </>
+  );
+};
 
 export default Sidebar;
